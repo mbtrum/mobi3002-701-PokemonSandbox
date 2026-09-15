66afe437b9642922c06b9be7acfa2adb4417d6ed
Michael Trumbull <michael.trumbull@nscc.ca>
2026-09-15 15:24:09 -0300

    Initial commit

---
+++ app/src/main/java/com/example/pokemonsandbox/MainActivity.kt
@@ -0,0 +1,66 @@
+package com.example.pokemonsandbox
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.pokemonsandbox.ui.theme.PokemonSandboxTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            PokemonSandboxTheme {
+                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+
+    Row(
+        modifier = Modifier
+            .padding(top=20.dp)
+            .fillMaxWidth()
+    )
+    {
+        Column()
+        {
+            Text(
+                text = "Hello $name!"
+            )
+        }
+        Column()
+        {
+            Text(
+                text = "Welcome to my app.",
+                modifier = Modifier.padding(start=10.dp)
+            )
+        }
+    }
+}
