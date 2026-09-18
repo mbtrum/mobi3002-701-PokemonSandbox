065dbd46c837b836220e2f73437c24d27317d892
Michael Trumbull <michael.trumbull@nscc.ca>
2026-09-18 10:17:10 -0300

    Added an image composable.

--- app/src/main/java/com/example/pokemonsandbox/MainActivity.kt
+++ app/src/main/java/com/example/pokemonsandbox/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
@@ -14,11 +15,13 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -26,6 +29,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -45,7 +49,8 @@ class MainActivity : ComponentActivity() {
 //                }
 
                 //ColumnDisplay()
-                FoodsLazyColumn()
+                //FoodsLazyColumn()
+                DisplayPokemon()
 
             }
         }
@@ -112,6 +117,33 @@ fun FoodsLazyColumn() {
         items(foods) { food ->
             Text(food)
         }
+    }
+}
+
+@Composable
+fun DisplayPokemon()
+{
+    // Align in centre of screen
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .fillMaxSize()
+    )
+    {
+        Image(
+            painter = painterResource(id = R.drawable.ditto),
+            contentDescription = "Ditto",
+            modifier = Modifier
+                .size(100.dp)
+        )
+
+        Text(text = "Ditto",
+            style = MaterialTheme.typography.headlineLarge
+        )
+
+        Text(text = "Limber / Imposter",
+            style = MaterialTheme.typography.bodyMedium)
     }
 }
 
